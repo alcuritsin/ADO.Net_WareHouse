8c3ef33730344ab858c5eb2332b77c906231a577
Курицын Алексей <al.curitsin@gmail.com>
2021-09-16 21:25:25 +0500

    Products.cs --add Добавление описания таблици продукты

---
+++ LibDB/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibDB
+{
+    public class Products
+    {
+        //  Продукт
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int TypeId { get; set; }
+        public int SupplierId { get; set; }
+        public double ProductQuantity { get; set; }
+        public double ProductCost { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
