de1d3c23c89d8c34c18cdc4ccf98aa51c8e6d5bc
Курицын Алексей <al.curitsin@gmail.com>
2021-09-16 21:18:06 +0500

    Inint

---
+++ LibDB/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using LibDB_Core;
+
+namespace LibDB
+{
+    public class DataBase:DB_connect
+    {
+        public static string Info()
+        {
+            return "Hello app!";
+        }
+    }
+}
---
+++ WareHouse_CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using LibDB;
+
+static void Main()
+{
+    var db = new DataBase();
+
+    db.Open();
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine(DataBase.Info());
+    Console.WriteLine(DataBase.InfoVersion());
+    db.Close();
+}
